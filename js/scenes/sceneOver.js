cf9b05970cbf5dd208b97b3b1871f42a2a52a6b8
NataJenkins <38480860+NataJenkins@users.noreply.github.com>
2021-06-22 16:30:14 -0500

    game over winner screen

--- js/scenes/sceneOver.js
+++ js/scenes/sceneOver.js
@@ -11,7 +11,22 @@ class SceneOver extends Phaser.Scene {
 
     var title = this.add.image(0, 0, "title");
     Align.scaleToGameW(title, 0.8);
-    this.alignGrid.placeAtIndex(38, title);
+    this.alignGrid.placeAtIndex(16, title);
+
+    this.winnerText = this.add.text(0, 0, "WINNER", {
+      fontSize: game.config.width / 10,
+      color: "green",
+    });
+    this.alignGrid.placeAtIndex(36, this.winnerText);
+
+    if (model.playerWon == true) {
+      this.winner = this.add.image(0, 0, "ship");
+    } else {
+      this.winner = this.add.image(0, 0, "eship");
+    }
+    Align.scaleToGameW(this.winner, 0.25);
+    this.winner.angle = 270;
+    this.alignGrid.placeAtIndex(60, this.winner);
 
     var btnStart = new FlatButton({
       scene: this,
@@ -19,7 +34,7 @@ class SceneOver extends Phaser.Scene {
       text: "Play Again!",
       event: "start_game",
     });
-    this.alignGrid.placeAtIndex(60, btnStart);
+    this.alignGrid.placeAtIndex(93, btnStart);
 
     emitter.on("start_game", this.startGame, this);
   }
