314e93d38ba144a583888d4eef3962458097b0a7
NataJenkins <38480860+NataJenkins@users.noreply.github.com>
2021-06-23 16:34:30 -0500

    main screen

--- js/scenes/sceneTitle.js
+++ js/scenes/sceneTitle.js
@@ -3,7 +3,6 @@ class SceneTitle extends Phaser.Scene {
     super("SceneTitle");
   }
   preload() {
-    this.load.image("title", "images/title.png");
     this.load.image("button1", "images/ui/buttons/2/1.png");
   }
   create() {
@@ -11,10 +10,6 @@ class SceneTitle extends Phaser.Scene {
     controller = new Controller();
 
     this.alignGrid = new AlignGrid({ rows: 11, cols: 11, scene: this });
-
-    var title = this.add.image(0, 0, "title");
-    Align.scaleToGameW(title, 0.8);
-    this.alignGrid.placeAtIndex(38, title);
 
     var btnStart = new FlatButton({
       scene: this,
@@ -25,7 +20,7 @@ class SceneTitle extends Phaser.Scene {
     this.alignGrid.placeAtIndex(60, btnStart);
 
     emitter.on("start_game", this.startGame, this);
-    this.scene.start("SceneMain");
+    //this.scene.start("SceneMain");
   }
   startGame() {
     this.scene.start("SceneMain");
